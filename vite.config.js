e169f9d9e6014f845dcf63463e049bdfe4cee984
AzizArres <63376723+AzizArres@users.noreply.github.com>
2024-08-22 14:20:13 +0100

    react modal fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ReactModal from "react-modal"
 
 // https://vitejs.dev/config/
 export default defineConfig({
