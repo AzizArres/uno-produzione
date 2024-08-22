902a814c1655d644cfd28054ad162ce024e72a0c
AzizArres <63376723+AzizArres@users.noreply.github.com>
2024-08-22 14:24:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ReactModal from "react-modal"
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-modal']
-    }
-  }
 })
